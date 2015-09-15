081d69251bde47ec753640be06f78a603e87c3c7
Matheus Rosa <matheusdsrosa@gmail.com>
2015-09-15 11:35:43 -0300

    Add create_tab_link function

--- popup.js
+++ popup.js
@@ -11,37 +11,41 @@ function go_to_tab(tab_id){
   });
 }
 
+function create_tab_link(tab){
+    $ul = $('ul#tabs');
+    
+    $li = $('<li></li>');
+    $li.attr({class: 'list-group-item'});
+
+    $link = $('<a></a>');
+    $link.attr({href: '#', id: tab.id});
+    $favicon = $('<img />');
+    $favicon.attr({src: tab.favIconUrl, id: 'favicon'});
+    
+    $link.append($favicon);
+    $link.append(tab.title);
+
+    $li.append($link);
+    $ul.append($li);
+}
+
 $(document).ready(function(){
   chrome.tabs.query({
     currentWindow: true
   }, function(tabs) {
-    var i, $ul = $('ul#tabs');
+    var i;
 
     for(i = 0; i < tabs.length; i++){
       tab = tabs[i];
-      tab_id = tab.id
-      tab_favicon_url = tab.favIconUrl
-      tab_title = tab.title
-
-      $li = $('<li></li>');
-      $li.attr({class: 'list-group-item'});
-
-      $link = $('<a></a>');
-      $link.attr({href: '#', id: tab_id});
-      $favicon = $('<img />');
-      $favicon.attr({src: tab_favicon_url, id: 'favicon'});
-      
-      $link.append($favicon);
-      $link.append(tab_title);
-
-      $li.append($link);
-      $ul.append($li);
+      create_tab_link(tab);
     }
 
     $('ul#tabs li a').each(function(index){
       $(this).click(function(){
-        go_to_tab(parseInt($(this).attr('id')));
+        tab_id = parseInt($(this).attr('id'));
+        go_to_tab(tab_id);
       });
     });
+
   });
 });
