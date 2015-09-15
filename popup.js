f5021c7d77f1ead7a881ca3f1436c4e70bacb8da
Matheus Rosa <matheusdsrosa@gmail.com>
2015-09-15 17:18:09 -0300

    Refactored code breaking into small functions

--- popup.js
+++ popup.js
@@ -4,12 +4,14 @@
  */
 
 var TITLE_LENGTH_LIMIT = 50;
+var favicon_url_regex = /localhost/i
 
 // labels
 var $pinned_label = $('<span class="label label-default">pinned</span>');
 var $active_label = $('<span class="label label-success">active</span>');
 var $audible_label = $('<span class="label label-primary">audible</span>');
 
+// tab functions
 function go_to_tab(tab_id){
   chrome.tabs.update(tab_id, {
     active: true
@@ -22,56 +24,71 @@ function close_tab(tab_id){
  chrome.tabs.remove(tab_id);
 }
 
-function apply_label_to_tab(li, tab){
-  if(tab.pinned == true)
-    $li.append($pinned_label);
+// list creation functions
+function add_favicon(li, favicon_url){
+  favicon = $('<img />');
   
-  if(tab.active == true)
-    $li.append($active_label);
+  if(favicon_url == undefined || favicon_url_regex.exec(favicon_url))
+    favicon_url = "page.png";
   
-  if(tab.audible == true)
-    $li.append($audible_label);
+  favicon.attr({src: favicon_url, id: 'favicon'});
+  
+  li.append(favicon);
 }
 
-function add_close_button_to_li(li, tab_id){
-  $button = $('<button></button>');
-  $button.attr({class: 'btn btn-danger btn-xs', id: tab_id, style: 'float: right', title: 'Close tab'});
-  $span = $('<span></span>');
-  $span.attr({class: 'glyphicon glyphicon-remove'});
-  $button.append($span);
+function add_title(link, tab_title){
+  title = tab_title
+  if(tab_title.length > TITLE_LENGTH_LIMIT)
+    title = tab_title.substring(0, TITLE_LENGTH_LIMIT) + '...';
   
-  $button.click(function(){
+  link.append(title);
+}
+
+function add_link(li, tab_id, tab_title){
+  link = $('<a></a>');
+  link.attr({href: '#', id: tab_id});
+  add_title(link, tab_title);
+  li.append(link);
+}
+
+function add_label(li, tab){
+  if(tab.pinned == true)
+    li.append($pinned_label);
+  
+  if(tab.active == true)
+    li.append($active_label);
+  
+  if(tab.audible == true)
+    li.append($audible_label);
+}
+
+function add_close_button(li, tab_id){
+  var button = $('<button></button>');
+  button.attr({class: 'btn btn-danger btn-xs', id: tab_id, style: 'float: right', title: 'Close tab'});
+  var span = $('<span></span>');
+  span.attr({class: 'glyphicon glyphicon-remove'});
+  button.append(span);
+
+  button.click(function(){
     close_tab(tab_id);
     li.fadeOut();
   });
 
-  li.append($button);
+  li.append(button);
 }
 
 function create_tab_link(tab){
-  $ul = $('ul#tabs');
+  ul = $('ul#tabs');
   
-  $li = $('<li></li>');
-  $li.attr({class: 'list-group-item'});
+  li = $('<li></li>');
+  li.attr({class: 'list-group-item'});
 
-  $link = $('<a></a>');
-  $link.attr({href: '#', id: tab.id});
-  $favicon = $('<img />');
-  $favicon.attr({src: tab.favIconUrl, id: 'favicon'});
-  
-  $link.append($favicon);
-  if(tab.title.length > TITLE_LENGTH_LIMIT){
-    $link.append(tab.title.substring(0, TITLE_LENGTH_LIMIT) + '...');
-  }else{
-    $link.append(tab.title);
-  }
+  add_favicon(li, tab.favIconUrl);
+  add_link(li, tab.id, tab.title);
+  add_label(li, tab);
+  add_close_button(li, tab.id);
 
-  $li.append($link);
-  
-  apply_label_to_tab($li, tab);
-  add_close_button_to_li($li, tab.id);
-
-  $ul.append($li);
+  ul.append(li);
 }
 
 $(document).ready(function(){
