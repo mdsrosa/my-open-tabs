5b0c14c6ba9a231016125b99a4886653197d33bc
Matheus Rosa <matheusdsrosa@gmail.com>
2015-09-14 22:52:46 -0300

    Add twitter bootstrap and improved popup layout

--- popup.js
+++ popup.js
@@ -15,11 +15,12 @@ $(document).ready(function(){
   chrome.tabs.query({
     currentWindow: true
   }, function(tabs) {
-    var i, $ul_tabs = $('ul#tabs');
+    var i, ul_tabs = $('ul#tabs');
 
     for(i = 0; i < tabs.length; i++){
       tab_id = tabs[i].id
-      $ul_tabs.append('<li><a href="#" id="'+tab_id+'">' + tabs[i].title + '</a></li>');
+      tab_favicon_url = tabs[i].favIconUrl
+      ul_tabs.append('<li><a href="#" id="'+tab_id+'" class="list-group-item"> '+'<img src="'+tab_favicon_url+'" id="favicon"/> '+ tabs[i].title + '</a></li>');
     }
 
     $('ul#tabs li a').each(function(index){
